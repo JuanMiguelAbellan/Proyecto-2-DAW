ec53a5461f69383468e4893e41b80f6207252af7
Juan Miguel Abellan <240023@fppiramide.com>
2026-01-11 11:29:58 +0100

    Archivos para CI-CD

--- .github/scripts/init-ec2-ollama.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-apt-get update -y
-apt-get install -y docker.io
-systemctl enable --now docker
-usermod -aG docker ubuntu
-docker run -d --restart always -p 11434:11434 --name ollama ollama/ollama:0.3.6-rocm
-sleep 30
-docker exec ollama ollama pull llama3.2:3b
