279a4133e5d50b9bdceaac0bfd338383fa49cec5
JuanMiguelAbellan <240023@fppiramide.com>
2026-01-19 11:52:45 +0100

    Echo boceto pagina principal

---
+++ frontend/script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", listeners)
+
+function listeners(){
+    button = document.querySelector(".enviar")
+    input = document.querySelector("#campo")
+    
+    button.addEventListener("click", function(e){
+        let text = input.value
+        crearMensaje("mensaje_usuario", text)
+        crearMensaje("mensaje_ia_wait")
+    })
+
+    input.addEventListener("keydown", function(e){
+        if(e.key === "Enter"){
+            let text = input.value
+            crearMensaje("mensaje_usuario", text)
+            crearMensaje("mensaje_ia_wait")
+        }
+    })
+}
+
+function crearMensaje(clase, contenido){
+    if(clase === "mensaje_ia_wait"){
+        mensaje = document.createElement("p")
+        span = document.createElement("span")
+        mensaje.setAttribute("class", clase)
+        mensaje.append(document.createElement("span"))
+        document.querySelector(".emoji").before(mensaje)
+        input.value = ""
+    }else{
+        mensaje = document.createElement("p")
+        mensaje.textContent = contenido
+        mensaje.setAttribute("class", clase)
+        document.querySelector(".emoji").before(mensaje)
+        input.value = ""
+    }
+}
