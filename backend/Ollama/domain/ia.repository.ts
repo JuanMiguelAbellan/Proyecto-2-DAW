a884e4ce8759c56578157a33da69a3edbf63041c
Juan Miguel Abellan <240023@fppiramide.com>
2026-02-13 20:40:15 +0100

    Falta terminar metodos ia.repository.Postgres

--- backend/Ollama/domain/ia.repository.ts
+++ backend/Ollama/domain/ia.repository.ts
@@ -2,9 +2,9 @@ import Chat from "../domain/Chat"
 import Mensaje from "../domain/Mensaje"
 export default interface IaReposiroty{
     //Hay que mirar que hacer con las preferencias y como tratarlas
-    crearChat(idUsuario: Number):Promise<void>
+    crearChat(idUsuario: Number):Promise<Number>
     addPreferencia(preferencia: String, idUsuario: Number): Promise<String>
     editPreferencia(preferencias: String, idUsuario: Number): Promise<String>
-    guardarRespuesta(respuesta: Mensaje, idChat?: Number): Promise<void>
-    guardarDocumentoRespuesta(documento: Mensaje, idChat?: Number): Promise<void>
+    guardarRespuesta(respuesta: Mensaje, idChat?: Number, idUsuario?:Number): Promise<void>
+    guardarDocumentoRespuesta(documento: Mensaje, idChat?: Number, idUsuario?:Number): Promise<void>
 }
--- backend/Ollama/infrastructure/db/ia.repository.Postgres.ts
+++ backend/Ollama/infrastructure/db/ia.repository.Postgres.ts
@@ -4,13 +4,27 @@ import Chat from "../../domain/Chat"
 import Mensaje from "../../domain/Mensaje"
 
 export default class IaRepositoryPostgres implements IaReposiroty{
-    async crearChat(idUsuario: Number ): Promise<void> {
+    async crearChat(idUsuario: Number ): Promise<Number> {
         throw new Error("Method not implemented.");
     }
-    async guardarRespuesta(respuesta: Mensaje, idChat?: Number): Promise<void> {
+    async guardarRespuesta(respuesta: Mensaje, idChat?: Number, idUsuario?:Number): Promise<void> {
+        let idChatNuevo
+        if(idChat == null && idUsuario != null){
+            idChatNuevo=this.crearChat(idUsuario)
+        }else{
+            idChatNuevo=idChat
+        }
+        const query=`'${idChatNuevo}'`
         throw new Error("Method not implemented.");
     }
-    async guardarDocumentoRespuesta(documento: Mensaje, idChat?: Number): Promise<void> {
+    async guardarDocumentoRespuesta(documento: Mensaje, idChat?: Number, idUsuario?:Number): Promise<void> {
+        let idChatNuevo
+        if(idChat == null && idUsuario != null){
+            idChatNuevo=this.crearChat(idUsuario)
+        }else{
+            idChatNuevo=idChat
+        }
+        const query=`'${idChatNuevo}'`
         throw new Error("Method not implemented.");
     }
     async addPreferencia(preferencia: String, id:Number): Promise<String> {
